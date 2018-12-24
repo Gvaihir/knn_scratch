caa2528b77e34deaf04c7f9cc5b9c47acfc3abfb
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2018-12-23 19:52:28 -0800

    v.0.1.1 Deployable, but slow. Avoid data frames structures

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 import pandas as pd
 irisIn = pd.read_csv('/Users/antonogorodnikov/Documents/Work/Python/knn_scratch/iris.data.txt', header=None)
 
-trainSet = irisIn.sample(frac=0.66, axis=0).reset_index(drop=True)
-testSet = irisIn[~irisIn.index.isin(trainSet.index)].reset_index(drop=True)
+trainSet = irisIn.sample(frac=0.66, axis=0)
+testSet = irisIn[~irisIn.index.isin(trainSet.index)]
+trainSet.reset_index(drop=True, inplace=True)
+testSet.reset_index(drop=True, inplace=True)
+
 
 print('Train set: {}\n'
       'Test set: {}'.format(len(trainSet),
@@ -24,21 +27,16 @@ def euclidDist(sample1, sample2, features):
 
 # getting neighbors
 
-def kNearesNeighbors(trainingSet, testSample, k):
+def kNearestNeighbors(testSample, trainingSet, k):
     distances = []
     length = len(testSample) - 1
     for i in range(len(trainingSet)):
         dist = euclidDist(testSample, trainingSet.iloc[i], features=length)
         distances.append(trainingSet.loc[i].append(pd.Series(dist, index=['dist'])))
         distDF = pd.DataFrame(distances)
-        distDF.sort_values(1, inplace=True)
-    return distDF.iloc[0:k]
-
-
-# measuring response
-
-def getResponse(neighbors):
-    classVotes = pd.DataFrame({'class': irisIn[4].unique(), 'vote': [0]*len(irisIn[4].unique())})
+        distDF.sort_values(by='dist', inplace=True)
+        neighbors = distDF.iloc[0:k]
+    classVotes = pd.DataFrame({'class': irisIn[4].unique(), 'vote': [0] * len(irisIn[4].unique())})
     for i in range(len(neighbors)):
         response = neighbors.iloc[i][-2]
         classVotes.loc[classVotes['class'] == response, 'vote'] += 1
@@ -47,6 +45,24 @@ def getResponse(neighbors):
     return response
 
 
+# true positive rate
+
+def tpr(testSet):
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet.iloc[i,4] is testSet.loc[i,'prediction']:
+            correct += 1
+    return correct/float(len(testSet))
 
 
+
+
+# deploy
+def main():
+    testSet['prediction'] = testSet.apply(kNearestNeighbors, trainingSet=trainSet, k=5, axis=1)
+
+
+tpr(testSet)
+
+print(timeit.timeit(lambda: main(),number=1))
 
